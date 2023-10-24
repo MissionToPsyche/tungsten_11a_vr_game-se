3482c1ee7e3b6f9016814ece0c1464a1de976b78
Garret Reichenbach <garret2kool@gmail.com>
2023-10-24 11:09:32 -0700

    WIP rework of camera focusing

--- Assets/Controller/Input/Interaction/ObjectFocuser.cs
+++ Assets/Controller/Input/Interaction/ObjectFocuser.cs
@@ -1,30 +1,11 @@
 using UnityEngine;
 
 namespace Controller.Input.Interaction {
-	
-	/**
-	* Interface for objects that can be highlighted / focused on when hovered over using the mouse or other input.
-	*/
-	public abstract class ObjectFocuser : MonoBehaviour {
-		
-		private Collider _collider;
-		private float _hoverTime;
+	public class ObjectFocuser : MonoBehaviour {
+		// Start is called before the first frame update
+		void Start() { }
 
-		void Start() {
-			_collider = GetComponent<Collider>();
-		}
-		
-		void Update() {
-			//Todo: IDE says that comparing to null is "expensive" and should be avoided, but I don't know how else to do this
-			if(_collider != null && _collider.bounds.Contains(InputManager.GetLookPosition())) {
-				_hoverTime += Time.deltaTime;
-				if(_hoverTime >= 5f) Focus(true);
-			} else {
-				_hoverTime = 0f;
-				Focus(false);
-			}
-		}
-
-		protected abstract void Focus(bool focused);
+		// Update is called once per frame
+		void Update() { }
 	}
 }
