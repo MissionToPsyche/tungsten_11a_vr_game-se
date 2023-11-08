f2270873673584d68e204b73f07fef387b65e2e5
eprosenha <119687711+eprosenha@users.noreply.github.com>
2023-11-08 12:02:50 -0700

    Revert "Rebasing onto dev/Fixing conflicts in Mercury scene"

---
+++ Assets/SceneClick.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class SceneClick : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    public string scene;
+
+    private void Awake()
+    {
+        //Empty
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        //Empty
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        //Empty
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        //Empty
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        //Empty
+    } 
+
+}
