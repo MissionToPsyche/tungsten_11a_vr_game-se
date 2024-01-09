d426b3508968ca4e49796eeff4498b08938977d7
Dallin Wallace <70779202+dkwalla1@users.noreply.github.com>
2024-01-08 18:16:48 -0700

    Attempting to check what devices are connected.

--- Assets/Scripts/ToggleMap.cs
+++ Assets/Scripts/ToggleMap.cs
@@ -1,11 +1,31 @@
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR;
 
 public class ToggleMap : MonoBehaviour
 {
     public GameObject ssMapObject;
 
     private bool mapVisible = false;
+
+    private List<InputDevice> devicesWithPrimaryButton;
+
+    void Start()
+    {
+        List<InputDevice> devices = new List<InputDevice>();
+        InputDevices.GetDevices(devices);
+
+        if (devices.Count == 0) {
+            Debug.Log("No VR devices have been detected in ToggleMap.cs");
+            return;
+        }
+
+        foreach (var device in devices)
+        {
+            //CheckPrimaryButton(device);
+            Debug.Log(string.Format("Device found with name '{0}' and characterisitcs '{1}'", device.name, device.characteristics.ToString()));
+        }
+    }
 
     void Update()
     {
