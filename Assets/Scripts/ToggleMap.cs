0fa5c168c04d4aafc2a6e901203137c61323e662
Dallin Wallace <70779202+dkwalla1@users.noreply.github.com>
2024-01-11 14:15:54 -0700

    Added VR mapping to solar system model toggling to all scenes

--- Assets/Scripts/ToggleMap.cs
+++ Assets/Scripts/ToggleMap.cs
@@ -1,7 +1,5 @@
 using System.Collections.Generic;
 using UnityEngine;
-// using UnityEngine.XR;
-// using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.InputSystem;
 
 public class ToggleMap : MonoBehaviour
@@ -9,26 +7,6 @@ public class ToggleMap : MonoBehaviour
     public GameObject ssMapObject;
 
     private bool mapVisible = false;
-
-   // private List<UnityEngine.XR.InputDevice> devices;
-
-    void Start()
-    {
-        // devices = new List<UnityEngine.XR.InputDevice>();
-        // InputDevices.GetDevices(devices);
-
-        // if (devices.Count == 0) {
-        //     Debug.Log("No VR devices have been detected in ToggleMap.cs");
-        //     return;
-        // }
-
-        // //if perfomance is an issue, try removing devices that don't have the characteristics we need
-        // foreach (var device in devices)
-        // {
-        //     //CheckPrimaryButton(device);
-        //     Debug.Log(string.Format("Device found with name '{0}' and characterisitcs '{1}'", device.name, device.characteristics.ToString()));
-        // }
-    }
 
     void Update()
     {
@@ -49,19 +27,6 @@ public class ToggleMap : MonoBehaviour
         mapVisible = !mapVisible;
         ssMapObject.SetActive(mapVisible);
     }
-
-    // private bool PrimaryKeyPressed() {
-    //     foreach (var device in devices)
-    //     {
-    //         bool isKeyPressed;
-    //         if (device.TryGetFeatureValue(UnityEngine.XR.CommonUsages.primaryButton, out isKeyPressed) && isKeyPressed)
-    //         {
-    //             Debug.Log("Toggle button is pressed.");
-    //             return true;
-    //         }
-    //     }
-    //     return false;
-    // }
 
 }
 
