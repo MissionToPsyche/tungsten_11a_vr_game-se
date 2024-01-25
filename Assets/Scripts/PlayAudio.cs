480421c58a8f77b463c409b47a1848c5ed6ff42b
Dallin Wallace <70779202+dkwalla1@users.noreply.github.com>
2024-01-24 18:31:53 -0700

    Fixed bug where audio get playing after the game has been paused

--- Assets/Scripts/PlayAudio.cs
+++ Assets/Scripts/PlayAudio.cs
@@ -7,18 +7,26 @@ public class PlayAudio : MonoBehaviour
 
      AudioSource source;
      private bool played;
+     private bool gameIsPaused;
 
     // Start is called before the first frame update
     void Start()
     {
         source = GetComponent<AudioSource>();
         played = false;
+        gameIsPaused = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        gameIsPaused = GameObject.Find("Canvas").GetComponent<PauseMenu>().isPaused;
+        if (gameIsPaused) {
+            source.Pause();
+        }
+        if (!gameIsPaused) {
+            source.UnPause();
+        }
     }
 
     private void OnTriggerEnter(Collider other)
