3b97a20d3b111da89ad8af1f48289bc4e8c95f83
smcochra1 <113396664+smcochra1@users.noreply.github.com>
2024-02-12 12:46:27 -0700

    Creating the event mode functionality
    This commit adds code to make the event mode menu functional. It will listen to the input, check the number of scenes entered, and if the input is accepted it will set event mode to true, and set the max number of scenes.

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -2,20 +2,34 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using TMPro;
+using System;
+using UnityEditor;
+
 
 public class MainMenuController : MonoBehaviour
 {
+    public GameObject mainMenu;
+    public GameObject eventModePopup;
     public Button freePlayButton;
     public Button eventGameButton;
     public Button settingsButton;
     public Button quitButton;
-    
+
+    public Button eventGoButton;
+    public Button eventModeBackButton;
+    public GameObject eventModeInput;
+
+    protected static Boolean eventMode = false;    // Keep track of if we are in event mode
+    protected static int maxScenes = 15;           // Max number of scenes a player can visit (to be used in event mode only)
+
     private void Start()
     {
         freePlayButton.onClick.AddListener(FreePlayStart);
-        eventGameButton.onClick.AddListener(EventGameStart);
+        eventGameButton.onClick.AddListener(EventModePopup);
         settingsButton.onClick.AddListener(SettingsScene);
         quitButton.onClick.AddListener(QuitGame);
+        eventGoButton.onClick.AddListener(EventModeGo);
+        eventModeBackButton.onClick.AddListener(EventModeBack);
     }
 
     public void FreePlayStart()
@@ -23,9 +37,44 @@ public class MainMenuController : MonoBehaviour
         SceneManager.LoadScene("Earth");
     }
 
-    public void EventGameStart() 
+    public void EventModePopup() 
     {
-        Debug.Log("Start event game! TBI");
+        mainMenu.SetActive(false);
+        eventModePopup.SetActive(true);
+    }
+
+    public void EventModeGo()
+    {
+        string inputText = eventModeInput.GetComponent<TMP_InputField>().text;
+        int inputNum = 0;
+        Int32.TryParse(inputText, out inputNum);
+        if (inputNum <= 0 || inputNum > 9)
+        {
+            EditorUtility.DisplayDialog("Warning",
+                "Please enter a number 1 through 9", "OK");
+        } else
+        {
+            maxScenes = inputNum;
+            eventMode = true;
+            SceneManager.LoadScene("Earth");
+            Debug.Log("Max Scenes: " + maxScenes);
+        }
+    }
+
+    public void EventModeBack()
+    {
+        eventModePopup.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+
+    public static int getMaxScenes()
+    {
+        return maxScenes;
+    }
+
+    public static Boolean isEventMode()
+    {
+        return eventMode;
     }
 
     public void SettingsScene()
