6906e8c2e13fc32e301182dc48d9ee6e9613ce1c
Garret Reichenbach <garret2kool@gmail.com>
2024-02-18 13:21:00 -0700

    UI cleanup and refactoring

---
+++ Assets/UI/Menus/EventMenu.cs
@@ -0,0 +1,7 @@
+namespace UI.Menus {
+	
+	public class EventMenu : GUIMenu {
+		protected override void Initialize() { throw new System.NotImplementedException(); }
+		protected override void UpdateMenu() { throw new System.NotImplementedException(); }
+	}
+}
---
+++ Assets/UI/Menus/MainMenu.cs
@@ -0,0 +1,12 @@
+namespace UI.Menus {
+
+	public class MainMenu : GUIMenu {
+		protected override void Initialize() {
+			
+		}
+
+		protected override void UpdateMenu() {
+			
+		}
+	}
+}
