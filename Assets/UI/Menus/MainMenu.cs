33530301f94ca78760527fc447a75c8dd0bc5931
Garret Reichenbach <garret2kool@gmail.com>
2024-03-02 18:14:14 -0700

    Worked on control menu setup

--- Assets/UI/Menus/MainMenu.cs
+++ Assets/UI/Menus/MainMenu.cs
@@ -5,16 +5,16 @@ namespace UI.Menus {
 
 	public class MainMenu : GUIMenu {
 		protected override void Initialize() {
-			transform.Find("FreePlayButton").GetComponent<Button>().onClick.AddListener(() => {
-				SceneController.LoadScene("Earth");
+			GameObject.Find("FreePlayButton").GetComponent<Button>().onClick.AddListener(() => {
+				SceneController.LoadScene(SceneController.Scene.Earth);
 			});
-			transform.Find("EventModeButton").GetComponent<Button>().onClick.AddListener(() => {
+			GameObject.Find("EventModeButton").GetComponent<Button>().onClick.AddListener(() => {
 				GetController().GetEventMenu().SetActive(true);
 			});
-			transform.Find("SettingsButton").GetComponent<Button>().onClick.AddListener(() => {
+			GameObject.Find("SettingsButton").GetComponent<Button>().onClick.AddListener(() => {
 				GetController().GetSettingsMenu().SetActive(true);
 			});
-			transform.Find("QuitButton").GetComponent<Button>().onClick.AddListener(() => {
+			GameObject.Find("QuitButton").GetComponent<Button>().onClick.AddListener(() => {
 				Debug.Log("[MainMenu] Quit Button pressed, exiting application...");
 				UnityEditor.EditorApplication.isPlaying = false;
 				Application.Quit();
